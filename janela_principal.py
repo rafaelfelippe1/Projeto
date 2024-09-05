a3e6a8ef86ed52740370e2ef8db8bfb2f71a7128
rafaelfelippe1 <rafaelfelippe1@gmail.com>
2024-09-05 11:15:16 -0300

    adicionado o botao de localizar cliente e alguns comentarios sobre  a funçao localizar cliente

--- janela_principal.py
+++ janela_principal.py
@@ -22,12 +22,14 @@ class Janela_Principal:
         self.btn_cadastrar_cliente = tk.Button(frame_botoes, text="Cadastro de cliente", command=self.cadastrar_cliente)
         self.btn_cadastrar_cliente.pack(side=tk.LEFT, padx=10)
 
+        self.btn_localizar_cliente = tk.Button(frame_botoes, text="Localizar cliente", command=self.localizar_cliente)
+        self.btn_localizar_cliente.pack(side=tk.LEFT, padx=10, pady=10)
+
         self.btn_registro_venda = tk.Button(frame_botoes, text="Registro de Venda", command=self.registro_venda)
         self.btn_registro_venda.pack(side=tk.LEFT, padx=10, pady=10)
 
         self.btn_localizar = tk.Button(frame_botoes, text="Localizar Venda", command=self.localizar_venda)
         self.btn_localizar.pack(side=tk.LEFT, padx=10, pady=10)
-
 
     def cadastrar_produto(self):
         """Abre a janela de vendas."""
@@ -58,6 +60,18 @@ class Janela_Principal:
             self.resultado_busca(itens_encontrados)
         else:
             messagebox.showinfo("Resultado", "Nenhum produto encontrado.")
+
+    def localizar_cliente(self):
+        #falta definir uma consulta dentro do banco para localizar o cliente pesquisado!
+        #gerando uma tabela com os clientes
+        #ou abrir uma outra janela que contenha todos os clientes do banco e podendo dentro dela filtar os clientes desejados
+
+        termo_busca = simpledialog.askstring("Buscar cliente", "Digite o nome do cliente:")
+        itens_encontrados = [item for item in self.items if termo_busca.lower() in item[1].lower()]
+        if itens_encontrados:
+            self.resultado_busca(itens_encontrados)
+        else:
+            messagebox.showinfo("Resultado", "Nenhum cliente encontrado!")
 
     def resultado_busca(self, itens_encontrados):
         janela_resultados = tk.Toplevel(self.root)
